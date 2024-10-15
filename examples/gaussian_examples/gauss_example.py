62316701adef29fa4244dd6a028a90f8958a98c6
mohan-s1 <mjspare01@gmail.com>
2024-10-14 20:07:29 -0400

    Misc. editings

--- examples/gaussian_examples/gauss_example.py
+++ examples/gaussian_examples/gauss_example.py
@@ -1,9 +1,12 @@
 import sys
 sys.path.append("/Users/mohan/pycchem")
 
-from pycchem.gaussian import *
+# from pycchem.gaussian import *
+import pycchem.gaussian as pcg
 import numpy as np
+import matplotlib.pyplot as plt
 from pathlib import Path
+
 
 script_dir = Path(__file__).parent # find parent folder of current file, `gaussian_example.py`
 
@@ -11,4 +14,14 @@ infile_path = script_dir / "1_nh3_3_h2o.log"
 
 temp = np.linspace(100, 300, 100)
 
-s_trans, s_vib, s_rot = calc_entropy(infile = infile_path, temperature = temp, use_low_freq = True)
+s_trans, s_vib, s_rot = pcg.calc_entropy(infile = infile_path, temperature = temp, use_low_freq = True) # all entropies in terms of J/mol*K
+
+plt.plot(temp, s_trans, label = 'Translation')
+plt.plot(temp, s_vib, label = 'Vibration')
+plt.plot(temp, s_rot, label = 'Rotation')
+plt.xlabel("Temperature [K]", fontsize = 14, fontweight = "bold")
+plt.ylabel(r"Entropy $ \mathbf{ \left[ \frac{J}{mol \cdot K} \right] } $", fontsize = 14, fontweight = "bold")
+plt.xticks(fontsize=12, weight = 'bold')  # Adjust the fontsize as needed (e.g., 12, 14)
+plt.yticks(fontsize=12, weight = 'bold')  # Adjust the fontsize as needed (e.g., 12, 14)
+plt.legend(frameon = False)
+plt.show();
