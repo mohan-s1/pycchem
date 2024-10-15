0f0db5a942d9adc0ef96791aae3a438ea33399cf
mohan-s1 <mjspare01@gmail.com>
2024-10-14 21:15:00 -0400

    Finished vasp example

--- examples/vasp_examples/vasp_example.py
+++ examples/vasp_examples/vasp_example.py
@@ -10,10 +10,19 @@ from pathlib import Path
 
 script_dir = Path(__file__).parent # find parent folder of current file, `gaussian_example.py`
 
-infile_path = script_dir / "1_nh3_3_h2o.log"
+infile_path = script_dir / "OUTCAR"
 
 temp = np.linspace(200, 500, 100)
 
-frequencies = pvv.vasp_parse_low_frequencies("infile")
+frequencies = pvv.vasp_parse_low_frequencies(infile_path)
+
+print(f"The energy of the optimized structure is {pvv.last_vasp_energy(infile_path)} eV")
 
 s_vib = pge.vibrational_entropy(frequencies_cm1 = frequencies, temperatures = temp)
+
+plt.plot(temp, s_vib)
+plt.xlabel("Temperature [K]", fontsize = 14, fontweight = "bold")
+plt.ylabel(r"Entropy $ \mathbf{ \left[ \frac{J}{mol \cdot K} \right] } $", fontsize = 14, fontweight = "bold")
+plt.xticks(fontsize=12, weight = 'bold')  # Adjust the fontsize as needed (e.g., 12, 14)
+plt.yticks(fontsize=12, weight = 'bold')  # Adjust the fontsize as needed (e.g., 12, 14)
+plt.show();
